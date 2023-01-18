27cebbe555bc4f6823741edf34a4134d8b05a5b0
Staluks <janchukssss@inbox.lv>
2023-01-18 09:46:18 +0200

    amended task(trimmed variable)

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,12 +2,11 @@ package com.epam.mjc.io;
 
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.HashMap;
 
 
 public class FileReader {
-
+    File file = new File("C:\\Users\\37128\\IdeaProjects\\stage1-module6-io-task1\\src\\main\\resources\\Profile.txt");
     public Profile getDataFromFile(File file) {
         String email= "";
         int age = 0;
@@ -27,17 +26,21 @@ public class FileReader {
                 String[] profileText = rows[i].split(" ");
                 gatavs.put(profileText[0],profileText[1]);
             }
-            email = gatavs.get("Email:");
-            age = Integer.parseInt(gatavs.get("Age:"));
-            name = gatavs.get("Name:");
-            phone = Long.valueOf(gatavs.get("Phone:"));
+            email = gatavs.get("Email:").trim();
+            age = Integer.parseInt(gatavs.get("Age:").trim());
+            name = gatavs.get("Name:").trim();
+            phone = Long.valueOf(gatavs.get("Phone:").trim());
+            System.out.println(email);
+            System.out.println(age);
+            System.out.println(name);
+            System.out.println(phone);
 
-        }catch (FileNotFoundException e){
+        }catch (FileNotFoundException e) {
             e.printStackTrace();
         }catch (IOException i){
             i.printStackTrace();
         }
 
-        return new Profile(name, age, email,phone);
+        return new Profile(name, age, email, phone);
     }
 }
